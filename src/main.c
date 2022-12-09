57a98435cfd08d84cf69da8a418b84845277ac58
Tyler Grunenwald <tgrunenwald@punchthrough.com>
2022-12-09 16:10:46 -0600

    feat: add blinky threads

--- src/main.c
+++ src/main.c
@@ -1,35 +1,53 @@
 #include <drivers/gpio.h>
 #include <kernel.h>
 
-/* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS 1000
+#define STACKSIZE 1024
+#define PRIORITY K_LOWEST_APPLICATION_THREAD_PRIO
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Alias led0 is undefined in device tree"
+#endif
+#if !DT_NODE_HAS_STATUS(LED1_NODE, okay)
+#error "Alias led1 is undefined in device tree"
+#endif
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
-void main(void) {
-    int ret;
+void blink_led(void *spec, void *period) {
+    const struct gpio_dt_spec *led = (struct gpio_dt_spec *)spec;
+    const int32_t blink_period = (int32_t)period;
+    int err;
 
-    if (!device_is_ready(led.port)) {
+    if (!device_is_ready(led->port)) {
+        printk("ERROR: %s not ready\n", led->port->name);
         return;
     }
 
-    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
-    if (ret < 0) {
+    err = gpio_pin_configure_dt(led, GPIO_OUTPUT_ACTIVE);
+    if (err < 0) {
+        printk("ERROR: failed to configure %s\n", led->port->name);
         return;
     }
 
-    while (1) {
-        ret = gpio_pin_toggle_dt(&led);
-        if (ret < 0) {
+    for (;;) {
+        err = gpio_pin_toggle_dt(led);
+        if (err < 0) {
             return;
         }
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(blink_period);
     }
 }
+
+K_THREAD_DEFINE(blink_led0_thread, STACKSIZE, blink_led, &led0, 100, NULL,
+                PRIORITY, 0, 0);
+K_THREAD_DEFINE(blink_led1_thread, STACKSIZE, blink_led, &led1, 1000, NULL,
+                PRIORITY, 0, 0);
